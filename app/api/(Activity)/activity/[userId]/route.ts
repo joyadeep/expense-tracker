8dafa881357ed6c6fb115986ce94b1bb76b6e041
joyadeep <dev.joyadeep@gmail.com>
2024-02-08 22:46:43 +0545

    Solved recent activities error

--- app/api/(Activity)/activity/[userId]/route.ts
+++ app/api/(Activity)/activity/[userId]/route.ts
@@ -9,7 +9,7 @@ export const GET = async(req:NextRequest,{params}:{params:{userId:string}})=>{
        
         const offset = ((typeof pageNumber === "string" ? parseInt(pageNumber) : pageNumber) - 1) * pageSize;
 
-        const totalCount= await prisma?.activity.count({where:{userId:params.userId}}) ?? 0
+        const totalCount= await db.activity.count({where:{userId:params.userId}}) ?? 0
         const totalPages= Math.ceil(totalCount/pageSize)
 
         const response= await db.activity.findMany({
@@ -23,6 +23,7 @@ export const GET = async(req:NextRequest,{params}:{params:{userId:string}})=>{
         }
         return NextResponse.json({message:"success",currentPage:pageNumber,totalPages:totalPages,data:response},{status:200})
     } catch (error) {
+        console.log("error",error)
         return NextResponse.json({message:"something went wrong",error:error},{status:500})
     }
 }
