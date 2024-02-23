b891e06239798e0fe52bec08294c964b89bc6c4c
joyadeep <dev.joyadeep@gmail.com>
2024-02-23 16:03:26 +0545

    Change url href to host name

--- app/api/(Auth)/forgot-password/route.ts
+++ app/api/(Auth)/forgot-password/route.ts
@@ -16,7 +16,7 @@ export const POST = async(req:NextRequest)=>{
 
         const token = jwt.sign({email:email},process.env.JWT_SECRET!,{expiresIn:"24h"})
 
-        const link = `${new URL(req.url).href}/reset-password?token=${token}`
+        const link = `https://${new URL(req.url).hostname}/reset-password?token=${token}`
 
         await db.resetPasswordRequest.create({
             data:{
@@ -34,20 +34,20 @@ export const POST = async(req:NextRequest)=>{
             }
         });
         
-       await transporter.sendMail({
-            from: 'dev.joyadeep@gmail.com',
-            to: email,
-            subject: 'Reset password',
-            html: `
-            <p>Hello,</p>
-            <p>We received a request to reset your password. If you initiated this request, please click on the link below to reset your password. This link is valid for 24 hours.</p>
-            <a href=${link}>Reset Password</a>
-            <p>If you did not request a password reset, you can safely ignore this email.</p>
-            <p>Please note that this link will expire after 24 hours. If you're unable to click the link, please copy and paste it into your browser's address bar.</p>
-        `,
-        })
+    //    await transporter.sendMail({
+    //         from: 'dev.joyadeep@gmail.com',
+    //         to: email,
+    //         subject: 'Reset password',
+    //         html: `
+    //         <p>Hello,</p>
+    //         <p>We received a request to reset your password. If you initiated this request, please click on the link below to reset your password. This link is valid for 24 hours.</p>
+    //         <a href=${link}>Reset Password</a>
+    //         <p>If you did not request a password reset, you can safely ignore this email.</p>
+    //         <p>Please note that this link will expire after 24 hours. If you're unable to click the link, please copy and paste it into your browser's address bar.</p>
+    //     `,
+    //     })
 
-        return NextResponse.json({message:"please check your email"},{status:200})
+        return NextResponse.json({message:"please check your email",data:link},{status:200})
     } catch (error) {
         return NextResponse.json({message:"Something went wrong"},{status:500})
     }
