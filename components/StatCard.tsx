89d90866632dab86f43b5b013dce300b3b2a466a
joyadeep <dev.joyadeep@gmail.com>
2024-03-04 11:25:09 +0545

    Fix stat card

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -11,14 +11,13 @@ interface Istatcard {
 
 const StatCard = ({icon,title,amount,flux}:Istatcard) => {
   return (
-    <div className=' p-6 border rounded-xl bg-card w-full hover:shadow-md cursor-pointer '>
+    <div className=' relative p-6 border rounded-xl bg-card w-full hover:shadow-md cursor-pointer '>
          <div className='w-full flex items-center justify-between'>
           <div className='text-sm font-medium text-foreground/70'>{title}</div>
          <div className='text-foreground/70'>{icon}</div>
          </div>
           <p className='text-2xl font-bold'>{currencyFormat(amount ?? 0)}</p>
-          <small className={cn('text-xs',flux && flux < 0 ? 'text-green-500' : 'text-red-500')}>{flux && `${flux} % from last month`}</small>
-          {/* // TODO : add icon and color also add + for positive */}
+          <small className={cn('text-xs absolute bottom-2 ',flux && flux < 0 ? 'text-green-500' : 'text-red-500')}>{flux && `${flux} % from last month`}</small>
     </div>
   )
 }
